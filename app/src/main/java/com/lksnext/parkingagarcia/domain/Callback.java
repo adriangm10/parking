22ccbabd7f1f929758e854be8429895784debe99
adrian <adrianciro@hotmail.es>
2024-05-09 14:52:35 +0200

    initial commit

---
+++ app/src/main/java/com/lksnext/parkingagarcia/domain/Callback.java
@@ -0,0 +1,6 @@
+package com.lksnext.parkingagarcia.domain;
+
+public interface Callback {
+    void onSuccess();
+    void onFailure();
+}
---
+++ app/src/main/java/com/lksnext/parkingagarcia/view/fragment/MainFragment.java
@@ -0,0 +1,23 @@
+package com.lksnext.parkingagarcia.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.lksnext.parkingagarcia.R;
+
+
+public class MainFragment extends Fragment {
+    public MainFragment() {
+        // Es necesario un constructor vacio
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Asignar la vista (layout) al fragmento
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
